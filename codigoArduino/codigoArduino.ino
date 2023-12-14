277b6b8b7b74f6bfd299073e0fa5c17d42589fcf
LabMakerIfesColatina <LabMakerIfesColatina@gmail.com>
2023-12-14 14:57:32 -0300

    first commit

---
+++ codigoArduino/codigoArduino.ino
@@ -0,0 +1,108 @@
+#include "src/domain/motor.h"
+
+
+#define pwm1 9 // Motor Direito
+#define ingA1 4 // Motor Direito
+#define ingB1 3 // Motor Direito
+
+#define pwm2 8 // Motor Esquerdo
+#define ingA2 6 // Motor Esquerdo
+#define ingB2 5 // Motor Esquerdo
+
+//mpu 6050
+
+Motor motorDireito(pwm1, ingA1, ingB1);
+Motor motorEsquerdo(pwm2, ingA2, ingB2);
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // tempo para andar uma cedula!
+  int tempo = 2200;
+  int i;
+  if (Serial.available() > 0) {
+    String stringSerial = Serial.readStringUntil('\n'); 
+    for (i = 0; i < stringSerial.length(); i++) {
+      controlarMovimento(stringSerial.charAt(i),tempo);
+      delay(1000); 
+    }
+  }
+}
+
+void controlarMovimento(char direcao, int tempo){
+  switch (direcao) {
+      case 'W':
+        //SEGUIR EM FRENTE
+        andarFrente(tempo); 
+        break;
+      case 'A':
+        //ESQUERDA 90º
+        virarEsquerda90(); 
+        break;
+      case 'S':
+        //SEGUIR PARA TRAS
+        andarTras(tempo); 
+        break;
+      case 'D':
+        //DIREITA 90º
+        virarDireita90(); 
+        break;
+      case 'P':
+        //PARAR
+        parar(); 
+        break;
+    }
+}
+
+void andarFrente(int tempo){
+  motorDireito.setPwm(128);
+  motorEsquerdo.setPwm(128);
+
+  motorDireito.rodarHorario();
+  motorEsquerdo.rodarAntiHorario();
+
+  delay(tempo);  
+  parar();
+}
+
+void andarTras(int tempo){
+  motorDireito.setPwm(128);
+  motorEsquerdo.setPwm(128);
+
+  motorDireito.rodarAntiHorario();
+  motorEsquerdo.rodarHorario();
+
+  delay(tempo);  
+  parar();
+}
+
+void virarEsquerda90(){
+  motorDireito.setPwm(255);
+  motorEsquerdo.setPwm(255);
+
+  motorDireito.rodarHorario();
+  motorEsquerdo.rodarHorario();
+        
+  delay(1200);  
+  parar();
+}
+
+void virarDireita90(){
+  motorDireito.setPwm(255);
+  motorEsquerdo.setPwm(255);
+
+  motorDireito.rodarAntiHorario();
+  motorEsquerdo.rodarAntiHorario();
+      
+  delay(1200);   
+  parar();
+}
+
+void parar(){
+  motorDireito.parar();
+  motorEsquerdo.parar();
+}
+
